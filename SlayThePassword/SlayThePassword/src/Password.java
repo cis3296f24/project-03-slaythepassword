c019effef0fc2dba8dda0fd8c8071c0a51bd0385
Jonathan Cherry <tuo97200@temple.edu>
2024-11-08 12:34:32 -0500

    Password Architecture
    - Added randomly selecting from classes using dynamic initialization
    - Added displaying the hint
    - Added validation of condition

--- SlayThePassword/SlayThePassword/src/Password.java
+++ SlayThePassword/SlayThePassword/src/Password.java
@@ -1,3 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+
+
 public class Password {
-    
+    public final String folder = "Conditions";
+    Conditions randCondition;
+    List<Conditions> conditions;
+
+    public Password(){
+        List<Conditions> conditions = loads();
+        Random random = new Random();
+        randCondition = conditions.get(random.nextInt(conditions.size()));
+    }
+
+
+
+    private List<Conditions> loads() throws Exception{
+        //initialize new ArrayList
+        List <Conditions> conditions = new ArrayList<>();
+        //initialize all files in a folder into an array
+        File Condfolder = new File(folder);
+        File[] files = Condfolder.listFiles((dir, name) -> name.endsWith(".class"));
+
+        for(File file: files){
+            String className = file.getName().substring(0, file.getName().length() - 6);
+
+            try {
+                //Dynamically load each class using the forName() function
+                Class<?> condClasses = Class.forName("conditions." + className);
+                
+                if(Condition.class.isAssignableFrom(condClasses)){
+                    Condition condition = (Condition) condClasses.getDeclaredConstructor().newInstance();
+                    conditions.add(condition);
+                }
+            } catch (Exception e) {
+                System.err.println("Unable to propperly load Condition");
+            }
+        }
+
+
+        return conditions;
+    }
+
+    public void validate(String userInput){
+        randCondition.checkCondition();
+    }
+
+    public void displayConditions(){
+        System.out.println("hint: "+ randCondition.hint());
+    }
+
+
 }
+
--- SlayThePassword/SlayThePassword/src/PasswordDemo.java
+++ SlayThePassword/SlayThePassword/src/PasswordDemo.java
@@ -30,3 +30,22 @@ public class PasswordDemo {
     }
     
 }
+
+/*
+ * public class PasswordDemo {
+
+    public static void main(String[] args) {
+
+        Password password = new Password();
+        password.displayConditions();
+
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+
+        password.validate(userInput);
+
+        input.close();    
+    }
+    
+}
+ */

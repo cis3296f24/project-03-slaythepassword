280f2174eb7e466de4284627b56deb223b8fde67
Jonathan Cherry <tuo97200@temple.edu>
2024-11-14 18:30:37 -0500

    Work (not working)
     - Added logo to UI
    - Sorted java classes into folders
    - Added logo PNG to resources folder & PrimaryController

---
+++ application/src/main/java/slaythepasswordfx/PrimaryController.java
@@ -0,0 +1,22 @@
+package slaythepasswordfx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafX.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class PrimaryController {
+
+    @FXML
+    private ImageView logo;
+
+    public void initializeImage(){
+        Image image = new Image(getClass().getResource("/images/Slay.The.Password.Logo.png").toExternalForm());
+        logo.setImage(image)
+    }
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}

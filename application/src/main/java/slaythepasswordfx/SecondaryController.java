536c56682c4d89ee9aec742c8622c7da919d67e9
Jonathan Cherry <tuo97200@temple.edu>
2024-11-21 20:20:02 -0500

    Changes
    - added javaFX

--- application/src/main/java/slaythepasswordfx/SecondaryController.java
+++ application/src/main/java/slaythepasswordfx/SecondaryController.java
@@ -2,11 +2,20 @@ package slaythepasswordfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+private TextField userInput;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+
+    @FXML
+    private void captureUserInput(){
+        userInput = userInput.getText();
     }
 }

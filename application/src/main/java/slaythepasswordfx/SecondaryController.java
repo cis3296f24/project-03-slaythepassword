1401f552171cedd77caa0619fa5a6ed93f370c82
Jonathan Cherry <tuo97200@temple.edu>
2024-11-22 11:48:37 -0500

    Added backend dependency
    - add visability to two projects
    + need to add .jar file to bin,
    - fix up secondary controller.

--- application/src/main/java/slaythepasswordfx/SecondaryController.java
+++ application/src/main/java/slaythepasswordfx/SecondaryController.java
@@ -1,28 +1,33 @@
 package slaythepasswordfx;
 
 import java.io.IOException;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 //essential FXML variables
-@FXML
-private TextField userInput;
-@FXML
-private Label HPLabel;
-@FXML
-private Label nameLabel;
-@FXML
-private ImageView logoImageView;
-
-//Essential Backend variables
-private IntegerProperty health = new SimpleIntegerProperty(/*User.getHealth()*/);
-private String maxHealth;
 
 
 public class SecondaryController {
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Label HPLabel;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private ImageView logoImageView;
+
+    //Essential Backend variables
+    private IntegerProperty health = new SimpleIntegerProperty(/*Gameloop.User.getHealth()*/);
+    private String maxHealth;
+
+
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -31,26 +36,26 @@ public class SecondaryController {
 
     @FXML
     public void initialize(){
-        healthLabel.textProperty().bind(health.asString("%d / "+ maxHealth));
-        nameLabel.setText(/*User.getName()*/);
-        Image logo = new Image(getClass()getResourceAsStream("/assets/Untitled_Artwork_27.PNG"));
+        HPLabel.textProperty().bind(health.asString("%d / "+ maxHealth));
+        nameLabel.setText("TEST TEST CHANGE");
+        Image logo = new Image(getClass().getResourceAsStream("/assets/Untitled_Artwork_27.PNG"));
+        logoImageView.setImage(logo);
     }
 
     @FXML
     private void captureUserInput(){
         String Input = userInput.getText();
 
-        if (true) {
+        /*if (true) {
             //Game loop?        
         }else{
-            /*
             if(user.getHealth() > 0){
                 //end screen
             }   
             else{
-                User.loseHP(1);*/
+                User.loseHP(1);
             //}
-        }
+        }*/
 
         userInput.clear();
 
